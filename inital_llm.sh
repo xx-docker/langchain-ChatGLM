d6a72037d2516b242b91e19421fd80529a99e0ed
Your Name <you@example.com>
2023-05-22 19:35:30 +0800

    -

--- inital_llm.sh
+++ inital_llm.sh
@@ -1,2 +1,9 @@
-# !/bin/bash 
+# !/bin/bash ``
+MODEL_DIR="/data/workdir/models"
+mkdir -p  $MODEL_DIR || echo "File exsit"
 
+cd $MODEL_DIR
+git lfs install --depth 1 https://huggingface.co/GanymedeNil/text2vec-large-chinese
+# git lfs install --depth 1 https://huggingface.co/THUDM/chatglm-6b
+# git lfs install --depth 1 https://huggingface.co/fnlp/moss-moon-003-sft-int8
+git lfs install --depth 1 https://huggingface.co/IDEA-CCNL/Ziya-LLaMA-13B-v1
